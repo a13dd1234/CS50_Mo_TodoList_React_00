de3c05ba4aecc7921954007d9c7aa7afb0cbfb53
a13dd1234 <53269307+a13dd1234@users.noreply.github.com>
2020-01-08 16:46:28 +0200

    (feat) Add todo Item Button

--- src/App.js
+++ src/App.js
@@ -4,22 +4,32 @@ import { render } from "react-dom";
 import Hello from "./Hello";
 import TodoList from "./TodoList";
 
+let id = 3;
 class App extends Component {
   constructor() {
     super();
     this.state = {
       todos: [
-        { id: 0, text: "My First Todo", Checked: false },
-        { id: 1, text: "My Second Todo", Checked: false },
-        { id: 2, text: "My Thired Todo", Checked: false }
+        { id: 0, text: "My First Todo", checked: false },
+        { id: 1, text: "My Second Todo", checked: false },
+        { id: 2, text: "My Thired Todo", checked: false }
       ]
     };
   }
+
+  addTodo() {
+    const text = prompt("Please Enter Todo Text!");
+    this.setState({
+      todos: [...this.state.todos, { id: id++, text: text, checked: false }]
+    });
+  }
+
   render() {
     return (
       <div>
         <Hello />
         <hr />
+        <button onClick={() => this.addTodo()}>Add Todo Item</button>
         <TodoList todos={this.state.todos} />
       </div>
     );
