dacbf72850eae8d85192c06e78d48dc6669b0b6c
a13dd1234 <53269307+a13dd1234@users.noreply.github.com>
2020-01-08 14:47:13 +0200

    (Fix) Hello Style className App

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import "./styles.css";
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="Hello">
         <Hello />
       </div>
     );
--- src/Hello.js
+++ src/Hello.js
@@ -1,9 +1,8 @@
 import React from "react";
-import "./styles.css";
 
 export default function Hello() {
   return (
-    <div className="Hello">
+    <div className="App">
       <h1>Hello CodeSandbox</h1>
       <hr />
       <h2>Start editing to see some magic happen!</h2>
